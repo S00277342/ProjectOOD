144d6583874d5ae11ac319f5d3f690ae414464c0
Titouan CABELLO <S00277342@atu.ie>
2025-02-26 11:58:03 +0000

    Add files via upload

---
+++ 6-1/6-1/Model1.edmx.sql
@@ -0,0 +1,82 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 02/26/2025 10:38:58
+-- Generated from EDMX file: C:\Users\TitouanCabello-STUDE\source\repos\6-1\6-1\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [Books];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Books'
+CREATE TABLE [dbo].[Books] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Title] nvarchar(max)  NOT NULL,
+    [AuthorId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Authors'
+CREATE TABLE [dbo].[Authors] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Books'
+ALTER TABLE [dbo].[Books]
+ADD CONSTRAINT [PK_Books]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Authors'
+ALTER TABLE [dbo].[Authors]
+ADD CONSTRAINT [PK_Authors]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [AuthorId] in table 'Books'
+ALTER TABLE [dbo].[Books]
+ADD CONSTRAINT [FK_BookAuthor]
+    FOREIGN KEY ([AuthorId])
+    REFERENCES [dbo].[Authors]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_BookAuthor'
+CREATE INDEX [IX_FK_BookAuthor]
+ON [dbo].[Books]
+    ([AuthorId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
